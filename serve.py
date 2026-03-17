8da73cc156cef7c29b30d7d324b7439b44f9939c
eortas <ortas.e@gmail.com>
2026-03-17 19:32:02 +0100

    v1

--- serve.py
+++ serve.py
@@ -1,6 +1,12 @@
+<<<<<<< HEAD
 import os
 from textual_serve.server import Server
 
 port = int(os.environ.get("PORT", 8000))
 server = Server("python app_render.py", host="0.0.0.0", port=port)
+=======
+from textual_serve.server import Server
+
+server = Server("python main.py")  # el comando que lanza tu app
+>>>>>>> a5a4cdd (v1)
 server.serve()
