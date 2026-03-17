a89f14ac5b373eee66ca8d2b8d484012dc7b68da
eortas <ortas.e@gmail.com>
2026-03-17 20:56:29 +0100

    serve

--- serve.py
+++ serve.py
@@ -1,9 +1,6 @@
-from textual_serve.server import Server
-
 import os
 from textual_serve.server import Server
 
 port = int(os.environ.get("PORT", 8000))
-server = Server("python main.py", port=port)
+server = Server("python main.py", host="0.0.0.0", port=port)
 server.serve()
-
