c2b84aad36db0e9e54e2f22eb33d7c108fbeffd6
eortas <ortas.e@gmail.com>
2026-03-17 21:07:36 +0100

    render

--- serve.py
+++ serve.py
@@ -2,5 +2,5 @@ import os
 from textual_serve.server import Server
 
 port = int(os.environ.get("PORT", 8000))
-server = Server("python main.py", host="0.0.0.0", port=port)
+server = Server("python app_render.py", host="0.0.0.0", port=port)
 server.serve()
