31e8ffcc14829303817cc867e1c13c7267eb77e1
eortas <ortas.e@gmail.com>
2026-03-18 01:15:10 +0100

    fix(deploy): usar sys.executable en serve.py

--- serve.py
+++ serve.py
@@ -1,6 +1,8 @@
 import os
+import sys
 from textual_serve.server import Server
 
-port = int(os.environ.get("PORT", 8000))
-server = Server("python app_render.py", host="0.0.0.0", port=port)
+port = int(os.environ.get("PORT", 10000))
+cmd = f"{sys.executable} app_render.py"
+server = Server(cmd, host="0.0.0.0", port=port)
 server.serve()
